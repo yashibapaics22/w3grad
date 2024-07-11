d54a5b46a36b65ed71615d685dbf9380a7e7e1e9
Yashi Bajpai <122154320+yashibapaics22@users.noreply.github.com>
2024-07-11 19:12:54 +0530

    Create zigzag_level_order_traversal.java

---
+++ Assignment/Assignment_3/zigzag_level_order_traversal.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        boolean leftToRight = true;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> ll = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode temp = q.poll();
+                if (leftToRight) {
+                    ll.add(temp.val);
+                } else {
+                    ll.add(0, temp.val); 
+                }
+                if (temp.left != null) {
+                    q.add(temp.left);
+                }
+                if (temp.right != null) {
+                    q.add(temp.right);
+                }
+            }
+            ans.add(ll);
+            leftToRight = !leftToRight; 
+        }
+        return ans;
+    }
+}
