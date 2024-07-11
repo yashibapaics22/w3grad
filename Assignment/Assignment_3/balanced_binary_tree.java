772cd0a6c7d31705277090d9afb5af19975144c5
Yashi Bajpai <122154320+yashibapaics22@users.noreply.github.com>
2024-07-11 19:15:32 +0530

    Create balanced_binary_tree.java

---
+++ Assignment/Assignment_3/balanced_binary_tree.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+       int k=length(root);
+       if (k==-1)
+       return false;
+       return true;
+    }
+    public static int length(TreeNode root){
+        if (root==null)
+        return 0;
+        int left=length(root.left);
+        int right=length(root.right);
+        if (left==-1 || right==-1)
+        return -1;
+        if (Math.abs(left-right)>1)
+        return -1;
+        return 1+Math.max(left,right);
+    }
+}
